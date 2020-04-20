cd52df8d63e10a1a2be3c6236bd8acef78433095
Iles-Asma <60738751+Iles-Asma@users.noreply.github.com>
2020-04-20 12:30:30 +0200

    .

--- Animation_Js/main.js
+++ Animation_Js/main.js
@@ -1,26 +1,27 @@
-noStroke(); 
+noStroke();
+var leftX = 120;
 var rightX = 300;
-var sunRadius = 100;   // coordonnee des nuage sur l'axe X
+var sunRadius = 100; // coordonnee des nuage sur l'axe X
 
-draw = function() {
+draw = function () {
     background(184, 236, 255);
- 
+
     fill(255, 170, 0);
-    ellipse(200, 100, sunRadius, sunRadius); // setup du soleil dans le cadre de l'exemple 
-    
+    ellipse(200, 100, sunRadius, sunRadius);
+
     // clouds 
-    fill(255, 255, 255); // setup couleur nuage 
+    fill(255, 255, 255);
     // left cloud
-    ellipse(leftX, 150, 126, 97); 
-    ellipse(leftX+62, 150, 70, 60);      // setup nuage gauche 
-    ellipse(leftX-62, 150, 70, 60);
-    
+    ellipse(leftX, 150, 126, 97);
+    ellipse(leftX + 62, 150, 70, 60);
+    ellipse(leftX - 62, 150, 70, 60);
+
     // right cloud
-    ellipse(rightX,100, 126, 97);
-    ellipse(rightX+62, 100, 70, 60);   //setup nuage droite
-    ellipse(rightX-62, 100, 70, 60);
-    
-    rightX +=1; // animation nuage direction la droite
-    leftX -=1; // aniamtion nuage direction la gauche
-    
+    ellipse(rightX, 100, 126, 97);
+    ellipse(rightX + 62, 100, 70, 60);
+    ellipse(rightX - 62, 100, 70, 60);
+
+    rightX += 1; // animation nuage direction la droite
+    leftX -= 1; // aniamtion nuage direction la gauche
+
 };
