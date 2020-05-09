1710339454f272f0aa9f5e946d47f925d2573bcf
pierreaverty2001 <pierre.averty2001@gmail.com>
2020-05-09 17:19:44 +0200

    tpaspret

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ $(function(){
 function buttonVisit () {
     $(".div-gauche").addClass("mouvLeft");
     $(".div-droite").addClass("mouvRight");
-    $(".main-div").addClass("accueilEnd")
+    $(".main-div").addClass("accueilEnd");
+    $("title").text("Bienvenue sur WWI !");
     setTimeout(function(){
         $(".main-div").remove();
-        $("title").text("Bienvenue sur WWI !");
     }, 4000);
     console.log("test");
 };
