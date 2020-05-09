3f129d1392180d94c819e195da2f083e5b5681f2
pierreaverty2001 <pierre.averty2001@gmail.com>
2020-05-09 17:08:15 +0200

    tpabo

--- main.js
+++ main.js
@@ -9,4 +9,8 @@ $(function(){
 function buttonVisit () {
     $(".div-gauche").addClass("mouvLeft");
     $(".div-droite").addClass("mouvRight");
+    setTimeout(function(){
+        $(".main-div").remove();
+    }, 3250);
+    console.log("test");
 };
