757b1d91166c530bffd65ca3126bd27500689583
Iles-Asma <60738751+Iles-Asma@users.noreply.github.com>
2020-06-02 23:05:36 +0200

    replace arbre

--- quartiers/mmi.php
+++ quartiers/mmi.php
@@ -23,13 +23,15 @@
         </div>
         <div class="mmi-page">
             <svg class="arbre-gauche" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 166.34 387.97">
-                <title>arbre gauche</title>
+                <title>Fichier 10arbre gauche</title>
                 <g id="Calque_2" data-name="Calque 2">
                     <g id="Calque_3" data-name="Calque 3">
                         <path
                             d="M89.25,14c-5.27.85-22.2,3.61-32,17-7.86,10.74-3.55,18-10,39-4.57,14.89-7.92,15.12-14,30-5.93,14.54-18.1,44.35-6,70,2.75,5.82,10,18.36,29,28,1.51,2.93,12,22.54,34,27,19,3.83,33.81-6.16,41-11,21.76-14.66,28.54-37.66,31-46,.75-2.53,1.68-18.56,3.5-50.5,1.09-18.94.72-25.78-1.5-34.5a80.91,80.91,0,0,0-15-30c-.94-3.79-5.68-21.17-23-32C110.19,10.92,94,13.19,89.25,14Z"
                             style="fill:#8beabf" />
                         <line x1="94.75" y1="79.97" x2="94.75" y2="387.97"
+                            style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3px" />
+                        <polyline points="71.75 111.47 94.75 131.85 115.75 106.47"
                             style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3px" />
                         <polyline points="61.75 158.47 94.75 183.95 126.75 149.47"
                             style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3px" />
@@ -60,55 +62,54 @@
 
             <!-- <div class="dut-mmi">
                 <h3>DUT MMI </h3> -->
-                <svg class="mmi-batiment open" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1335.56 794.51">
-                    <title>DUT MMI</title>
-                    <g id="Calque_2" data-name="Calque 2">
-                        <g id="Calque_2-2" data-name="Calque 2">
-                            <rect x="169.73" y="190.93" width="1004.06" height="600.71" style="fill:#5765b2" />
-                            <rect x="138.16" y="724.17" width="1059.24" height="68.26" style="fill:#5765b2" />
-                            <rect x="470.55" y="118.67" width="408.8" height="114.01" style="fill:#5765b2" />
-                            <polygon points="470.55 118.67 671.37 23.1 879.35 118.67 470.55 118.67"
-                                style="fill:#5765b2" />
-                            <polygon
-                                points="1017.35 190.72 1173.7 191.1 1173.7 543.1 1092.7 543.46 1092.7 281.1 1017.7 279.1 1017.35 190.72"
-                                style="fill:#ffd985" />
-                            <polygon
-                                points="138.16 792.43 137.7 724.1 169.34 724.17 169.34 524.49 322.7 524.1 322.7 682.1 371.7 682.1 372.01 792.1 138.16 792.43"
-                                style="fill:#ffd985" />
-                            <ellipse cx="672.55" cy="170.98" rx="57.26" ry="55.45" style="fill:#fff" />
-                            <rect x="262.22" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="372.01" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="481.81" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="262.22" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="372.01" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="481.81" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="808.19" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="917.98" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="1027.78" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="808.19" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="917.98" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
-                            <rect x="1027.78" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
-                            <path d="M738.7,792.41H606.4V637.09c8-28.23,35.25-48,66.15-48s58.15,19.77,66.15,48Z"
-                                style="fill:#fff" />
-                            <polyline points="672.55 141.71 672.55 166.36 691.64 191"
-                                style="fill:none;stroke:#232323;stroke-linecap:round;stroke-miterlimit:10;stroke-width:3px" />
-                            <line y1="793.51" x2="1335.56" y2="793.51"
-                                style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:2px" />
-                            <rect x="581.28" y="519.1" width="173" height="47" style="fill:#ffd985" />
-                            <path
-                                d="M642.51,543.79l9.06-17.73h6.27v32.53h-6.32V538.41l-7.07,14.34h-4l-7-14.2v20h-6.27V526.06h6.18Z"
-                                style="fill:#fff" />
-                            <path
-                                d="M679.57,543.79l9-17.73h6.27v32.53h-6.31V538.41l-7.08,14.34h-4l-7-14.2v20h-6.28V526.06h6.18Z"
-                                style="fill:#fff" />
-                            <path d="M708.37,558.59h-6.6V526.06h6.6Z" style="fill:#fff" />
-                        </g>
-                        <g id="Calque_3" data-name="Calque 3">
-                            <polyline points="454.79 176.1 454.79 107.1 673.79 1.1 759.79 40.1"
-                                style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:2px" />
-                        </g>
+            <svg class="mmi-batiment open" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1335.56 794.51">
+                <title>DUT MMI</title>
+                <g id="Calque_2" data-name="Calque 2">
+                    <g id="Calque_2-2" data-name="Calque 2">
+                        <rect x="169.73" y="190.93" width="1004.06" height="600.71" style="fill:#5765b2" />
+                        <rect x="138.16" y="724.17" width="1059.24" height="68.26" style="fill:#5765b2" />
+                        <rect x="470.55" y="118.67" width="408.8" height="114.01" style="fill:#5765b2" />
+                        <polygon points="470.55 118.67 671.37 23.1 879.35 118.67 470.55 118.67" style="fill:#5765b2" />
+                        <polygon
+                            points="1017.35 190.72 1173.7 191.1 1173.7 543.1 1092.7 543.46 1092.7 281.1 1017.7 279.1 1017.35 190.72"
+                            style="fill:#ffd985" />
+                        <polygon
+                            points="138.16 792.43 137.7 724.1 169.34 724.17 169.34 524.49 322.7 524.1 322.7 682.1 371.7 682.1 372.01 792.1 138.16 792.43"
+                            style="fill:#ffd985" />
+                        <ellipse cx="672.55" cy="170.98" rx="57.26" ry="55.45" style="fill:#fff" />
+                        <rect x="262.22" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="372.01" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="481.81" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="262.22" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="372.01" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="481.81" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="808.19" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="917.98" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="1027.78" y="309.61" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="808.19" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="917.98" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
+                        <rect x="1027.78" y="485.21" width="45.57" height="101.66" style="fill:#fff" />
+                        <path d="M738.7,792.41H606.4V637.09c8-28.23,35.25-48,66.15-48s58.15,19.77,66.15,48Z"
+                            style="fill:#fff" />
+                        <polyline points="672.55 141.71 672.55 166.36 691.64 191"
+                            style="fill:none;stroke:#232323;stroke-linecap:round;stroke-miterlimit:10;stroke-width:3px" />
+                        <line y1="793.51" x2="1335.56" y2="793.51"
+                            style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:2px" />
+                        <rect x="581.28" y="519.1" width="173" height="47" style="fill:#ffd985" />
+                        <path
+                            d="M642.51,543.79l9.06-17.73h6.27v32.53h-6.32V538.41l-7.07,14.34h-4l-7-14.2v20h-6.27V526.06h6.18Z"
+                            style="fill:#fff" />
+                        <path
+                            d="M679.57,543.79l9-17.73h6.27v32.53h-6.31V538.41l-7.08,14.34h-4l-7-14.2v20h-6.28V526.06h6.18Z"
+                            style="fill:#fff" />
+                        <path d="M708.37,558.59h-6.6V526.06h6.6Z" style="fill:#fff" />
                     </g>
-                </svg>
+                    <g id="Calque_3" data-name="Calque 3">
+                        <polyline points="454.79 176.1 454.79 107.1 673.79 1.1 759.79 40.1"
+                            style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:2px" />
+                    </g>
+                </g>
+            </svg>
 
             <!-- </div> -->
 
@@ -150,18 +151,18 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-<script type="text/javascript">
-    $(document).ready(function () {
-        var navigateur = navigator.userAgent;
-        $(".open").click(function () {
-            $(".popup").show();
-        });
-        $(".background, .close").click(function () {
-            $(".popup").hide();
-        });
+    <script type="text/javascript">
+        $(document).ready(function () {
+            var navigateur = navigator.userAgent;
+            $(".open").click(function () {
+                $(".popup").show();
+            });
+            $(".background, .close").click(function () {
+                $(".popup").hide();
+            });
 
-    });
-</script>
+        });
+    </script>
 
 
 </body>
