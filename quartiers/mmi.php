a119d2ff34d3a438ab412f89ab8f0218db17e917
Iles-Asma <60738751+Iles-Asma@users.noreply.github.com>
2020-06-02 15:17:40 +0200

    importattio fichier svg mmi
    vous les retrouverre dans le dossier svg du projet

---
+++ quartiers/mmi.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Développement</title>
+    <link href="style.css" rel="stylesheet">
+    <script src="../main.js"></script>
+</head>
+
+<body class="dev">
+    <section>
+        <a href="../index.html">
+            <h1> &xlarr; Retour </h1>
+        </a>
+        <div class="popup">
+            <div class="background"></div>
+            <div class="info">
+                <button class="close">&times;</button>
+                <h3>Titre</h3>
+                <p>Text</p>
+            </div>
+        </div>
+
+
+
+
+
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 306.41 176.1">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: none;
+                        stroke: #fff;
+                        stroke-miterlimit: 10;
+                        stroke-width: 2px;
+                    }
+                </style>
+            </defs>
+            <title>trait haut mmi</title>
+            <g id="Calque_2" data-name="Calque 2">
+                <g id="Calque_3" data-name="Calque 3">
+                    <polyline class="cls-1" points="1 176.1 1 107.1 220 1.1 306 40.1" />
+                </g>
+            </g>
+        </svg>
+
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 86.51 210.71">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: none;
+                        stroke: #fff;
+                        stroke-miterlimit: 10;
+                        stroke-width: 2px;
+                    }
+                </style>
+            </defs>
+            <title>trait arbre gauche</title>
+            <g id="Calque_2" data-name="Calque 2">
+                <g id="Calque_3" data-name="Calque 3">
+                    <path class="cls-1"
+                        d="M86.25,1C69,5.56,46.38,14.74,36.25,35c-5.18,10.34-3.17,15.72-6,32-7,40.24-26.5,49.36-29,73-1.65,15.63,4,38.77,39,70" />
+                </g>
+            </g>
+        </svg>
+
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.26 374.78">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: #8beabf;
+                    }
+
+                    .cls-2 {
+                        fill: none;
+                        stroke: #fff;
+                        stroke-miterlimit: 10;
+                        stroke-width: 3px;
+                    }
+                </style>
+            </defs>
+            <title>arbre gauche</title>
+            <g id="Calque_2" data-name="Calque 2">
+                <g id="Calque_3" data-name="Calque 3">
+                    <path class="cls-1"
+                        d="M67.17.78c-5.28.85-22.21,3.61-32,17-7.86,10.74-3.55,18-10,39-4.57,14.89-7.93,15.12-14,30-5.94,14.54-18.11,44.35-6,70,2.75,5.81,10,18.36,29,28,1.51,2.93,12,22.54,34,27,19,3.83,33.8-6.16,41-11,21.75-14.66,28.54-37.66,31-46,.74-2.53,1.67-18.56,3.5-50.5,1.08-18.94.72-25.78-1.5-34.5a81.06,81.06,0,0,0-15-30c-.94-3.79-5.68-21.17-23-32C88.11-2.27,72,0,67.17.78Z" />
+                    <line class="cls-2" x1="72.67" y1="66.78" x2="72.67" y2="374.78" />
+                    <polyline class="cls-2" points="49.67 98.28 72.67 118.66 93.67 93.28" />
+                    <polyline class="cls-2" points="39.67 145.28 72.67 170.76 104.67 136.28" />
+                </g>
+            </g>
+        </svg>
+
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1335.56 771.4">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: #5765b2;
+                    }
+
+                    .cls-2 {
+                        fill: #ffd985;
+                    }
+
+                    .cls-3 {
+                        fill: #fff;
+                    }
+
+                    .cls-4,
+                    .cls-5 {
+                        fill: none;
+                        stroke-miterlimit: 10;
+                    }
+
+                    .cls-4 {
+                        stroke: #232323;
+                        stroke-linecap: round;
+                        stroke-width: 3px;
+                    }
+
+                    .cls-5 {
+                        stroke: #fff;
+                        stroke-width: 2px;
+                    }
+                </style>
+            </defs>
+            <title>mmi</title>
+            <g id="Calque_2" data-name="Calque 2">
+                <g id="Calque_2-2" data-name="Calque 2">
+                    <rect class="cls-1" x="169.73" y="167.83" width="1004.06" height="600.71" />
+                    <rect class="cls-1" x="138.16" y="701.07" width="1059.24" height="68.26" />
+                    <rect class="cls-1" x="470.55" y="95.57" width="408.8" height="114.01" />
+                    <polygon class="cls-1" points="470.55 95.57 671.37 0 879.35 95.57 470.55 95.57" />
+                    <polygon class="cls-2"
+                        points="1017.35 167.62 1173.7 168 1173.7 520 1092.7 520.35 1092.7 258 1017.7 256 1017.35 167.62" />
+                    <polygon class="cls-2"
+                        points="138.16 769.33 137.7 701 169.34 701.07 169.34 501.39 322.7 501 322.7 659 371.7 659 372.01 769 138.16 769.33" />
+                    <ellipse class="cls-3" cx="672.55" cy="147.87" rx="57.26" ry="55.45" />
+                    <rect class="cls-3" x="262.22" y="286.51" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="372.01" y="286.51" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="481.81" y="286.51" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="262.22" y="462.11" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="372.01" y="462.11" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="481.81" y="462.11" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="808.19" y="286.51" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="917.98" y="286.51" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="1027.78" y="286.51" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="808.19" y="462.11" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="917.98" y="462.11" width="45.57" height="101.66" />
+                    <rect class="cls-3" x="1027.78" y="462.11" width="45.57" height="101.66" />
+                    <path class="cls-3" d="M738.7,769.31H606.4V614c8-28.22,35.25-48,66.15-48s58.15,19.78,66.15,48Z" />
+                    <polyline class="cls-4" points="672.55 118.61 672.55 143.25 691.64 167.9" />
+                    <line class="cls-5" y1="770.4" x2="1335.56" y2="770.4" />
+                </g>
+            </g>
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 126">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: #d0c3e5;
+                    }
+
+                    .cls-2 {
+                        fill: none;
+                        stroke: #fff;
+                        stroke-miterlimit: 10;
+                        stroke-width: 2px;
+                    }
+                </style>
+            </defs>
+            <title>socle_mmi</title>
+            <g id="Calque_2" data-name="Calque 2">
+                <g id="Calque_1-2" data-name="Calque 1">
+                    <rect class="cls-1" width="1920" height="126" />
+                </g>
+                <g id="Calque_3" data-name="Calque 3">
+                    <line class="cls-2" x1="763" y1="28" x2="1503" y2="28" />
+                    <line class="cls-2" x1="519" y1="63" x2="1027" y2="63" />
+                </g>
+            </g>
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.15 414">
+            <defs>
+                <style>
+                    .cls-1 {
+                        fill: #8beabf;
+                    }
+
+                    .cls-2 {
+                        fill: none;
+                        stroke: #fff;
+                        stroke-miterlimit: 10;
+                        stroke-width: 3px;
+                    }
+                </style>
+            </defs>
+            <title>arbre droite </title>
+            <g id="Calque_2" data-name="Calque 2">
+                <g id="Calque_3" data-name="Calque 3">
+                    <path class="cls-1"
+                        d="M56.26.69c28.09-4.83,60.35,16.54,72.89,40.11s-5.66,28.28,0,83.64a541.11,541.11,0,0,0,11.12,67.75c2.16,8.73,3.24,11.57,3.62,17.6.56,9.06,2.3,36.93-22.44,58.89-5.23,4.63-25.72,22.82-51.3,21.33-36.28-2.1-63.41-42.65-67.32-75.95-.89-7.6.08-12.49.91-19.55C12.67,118.4-11.21,94.75,7.15,51,12.1,39.25,26.13,5.87,56.26.69Z" />
+                    <line class="cls-2" x1="69.15" y1="68" x2="69.15" y2="414" />
+                    <polyline class="cls-2" points="49.65 96.5 69.07 130.89 93.65 96.5" />
+                    <polyline class="cls-2" points="48.65 173.5 68.07 213.32 101.65 173.5" />
+                </g>
+            </g>
+        </svg>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
