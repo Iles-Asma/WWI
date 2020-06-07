bc3eb597fb5bb1da0da1240cf3b25d20a1bd7b78
ttl309 <62150976+ttl309@users.noreply.github.com>
2020-06-07 15:01:12 +0200

    Update audiovisuel.php

--- quartiers/audiovisuel.php
+++ quartiers/audiovisuel.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Audiovisuel</title>
     <link href="style.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="../images/favicon.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="../main.js"></script>
     <?php $db = new PDO ('mysql:host=localhost;dbname=wwi', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));?>
