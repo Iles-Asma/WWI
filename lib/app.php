e98e6bba5664fe5d3450e57b7c2e427df499822e
Iles-Asma <60738751+Iles-Asma@users.noreply.github.com>
2020-11-08 19:01:45 +0100

    Update app.php

--- lib/app.php
+++ lib/app.php
@@ -1,5 +1,5 @@
 <?php
-  define('DB_HOST', 'mysql:host=localhost;dbname=your_db_name');
+  define('DB_HOST', 'mysql:host=localhost;dbname=wwi');
   define('DB_USER', 'root');
   define('DB_PASSWORD', '');
 ?>
